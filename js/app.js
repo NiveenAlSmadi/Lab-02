843c1990df8a69e55515cf52ea63de688f723dbf
NiveenALSmadi <niveensmadi33@gmail.com>
2021-04-21 12:35:17 +0300

    try

--- js/app.js
+++ js/app.js
@@ -15,43 +15,48 @@ function X(value){
 }
 
 // git data from json
+const Page1='./data/page-1.json';
+const Page2='./data/page-2.json';
+function data1(){
 
-// function data1(){
-
-$.ajax('./data/page-1.json').then((data) => {
-  data.forEach((animals) => {
-    let title = new X(animals);
-    // console.log(title);
-    title.render();
+  $.ajax(Page1).then((data) => {
+    data.forEach((animals) => {
+      let title = new X(animals);
+      // console.log(title);
+      title.render();
     // title.optionRender();
     // sortByname(data);
+    });
   });
-});
+}
 
-// data1();
-// function data2(){
 
-$.ajax('./data/page-2.json').then((data) => {
-  data.forEach((animals) => {
-    let title = new X(animals);
-    // console.log(title);
-    title.render();
-    title.optionRender();
+function data2(){
+  $.ajax(Page2).then((data) => {
+    data.forEach((animals) => {
+      let title = new X(animals);
+      // console.log(title);
+      title.render();
+      title.optionRender();
     // sortByhorn(data);
+    });
   });
-});
+}
 
-
-// data2();
+data2();
 
 //render  //
 X.prototype.render = function() {
-  let animal = $('#photo-template').clone();
-  animal.find('h2').text(this.name);
-  animal.find('img').attr('src',`${this.src}`);
-  animal.addClass( this.keyword );
-  animal.find('p').text(this.detals);
-  $('main').append(animal);
+  // let animal = $('#photo-template').clone();
+  // animal.find('h2').text(this.name);
+  // animal.find('img').attr('src',`${this.src}`);
+  // animal.addClass( this.keyword );
+  // animal.find('p').text(this.detals);
+  // $('main').append(animal);
+
+  let template = $('.photo-template').html();
+  let gallaryTemplate = Mustache.render(template,this);
+  $('main').append(gallaryTemplate);
 
   this.optionRender();
 
@@ -85,40 +90,48 @@ $( '.keyword' ).change( ( e ) => {
 // let renderTemplate=Mustache.render(Template,this);
 // $('main').append(renderTemplate);
 
-$( '#page1' ).change( ( e ) => {
-  $( 'div' ).hide();
-  let targetValue = e.target.value;
-  $( `.${targetValue}` ).show();
-  if( targetValue === 'default' ){
-    $( 'div' ).show();
+// $( '#page1' ).change( ( e ) => {
+//   $( 'div' ).hide();
+//   let targetValue = e.target.value;
+//   $( `.${targetValue}` ).show();
+//   if( targetValue === 'default' ){
+//     $( 'div' ).show();
+//   }
+// } );
+
+
+// sortByname();
+
+// function sortByname(arr) {
+//   arr.sort((a, b) => {
+//     if (a.title.toUpperCase() < b.title.toUpperCase()) {
+//       return -1;
+//     } else if (a.title.toUpperCase() > b.title.toUpperCase()) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+//   return arr;
+// }
+
+// function sortByhorn(arr) {
+//   arr.sort((a, b) => {
+//     if (a.horns < b.horns) {
+//       return -1;
+//     } else if (a.horns > b.horns) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+//   return arr;
+// }
+
+// sortByhorn()
+$('.page').click((e)=>{
+  let numOfPage=e.target.value;
+  if(numOfPage==='page1'){
+    data1(Page1);
   }
-} );
-
-
-sortByname();
-
-function sortByname(arr) {
-  arr.sort((a, b) => {
-    if (a.title.toUpperCase() < b.title.toUpperCase()) {
-      return -1;
-    } else if (a.title.toUpperCase() > b.title.toUpperCase()) {
-      return 1;
-    }
-    return 0;
-  });
-  return arr;
-}
-
-function sortByhorn(arr) {
-  arr.sort((a, b) => {
-    if (a.horns < b.horns) {
-      return -1;
-    } else if (a.horns > b.horns) {
-      return 1;
-    }
-    return 0;
-  });
-  return arr;
-}
-
-sortByhorn()
+  if(numOfPage==='page2'){
+    data2(Page2);
+  }});
